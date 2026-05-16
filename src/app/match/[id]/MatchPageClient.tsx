68008e9d9b59423b5ef48d1cb3e9ac4b695ca55a
Juan Manuel <bara1021@gmail.com>
2026-05-15 21:17:11 -0500

    feat: remove guard to match register

--- src/app/match/[id]/MatchPageClient.tsx
+++ src/app/match/[id]/MatchPageClient.tsx
@@ -1,16 +1,13 @@
 'use client'
 
-import { AuthGuard } from '@/components/AuthGuard'
 import MatchDetails from '@/components/MatchDetails'
 
 export default function MatchPageClient({ matchId }: { matchId: string }) {
   return (
-    <AuthGuard>
-      <div className="min-h-screen bg-background py-12 px-4">
-        <div className="mx-auto">
-          <MatchDetails matchId={matchId} />
-        </div>
+    <div className="min-h-screen bg-background py-12 px-4">
+      <div className="mx-auto">
+        <MatchDetails matchId={matchId} />
       </div>
-    </AuthGuard>
+    </div>
   )
 }
